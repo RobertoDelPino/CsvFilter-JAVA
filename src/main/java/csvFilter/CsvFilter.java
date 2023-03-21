dbccd00a1c4d27754ff0231988f07ff3a4cc20b4
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-03-21 12:48:39 +0000

    Test(green): shouldExcludeLinesWithBothTaxFieldAreNotEmpty check if some tax fields is not empty to add to the result list

--- src/main/java/csvFilter/CsvFilter.java
+++ src/main/java/csvFilter/CsvFilter.java
@@ -1,6 +1,8 @@
 package csvFilter;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class CsvFilter {
     public List<String> apply(List<String> lines) throws Exception {
@@ -11,6 +13,19 @@ public class CsvFilter {
         if(lines.size() == 1){
             throw new Exception("Invalid file");
         }
-        return lines;
+
+        List<String> result = new ArrayList<>();
+        result.add(lines.get(0));
+
+        int indexIVA = 4;
+        int indexIGIC = 5;
+
+        String[] lineSplit = lines.get(1).split(",");
+
+        if(lineSplit[indexIVA].equals("") || lineSplit[indexIGIC].equals("")){
+            result.add(lines.get(1));
+        }
+
+        return result;
     }
 }
