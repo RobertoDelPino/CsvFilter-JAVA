ce6437106c5a1b41587670227c60c0b5f71eef0e
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-03-21 11:51:14 +0000

    Test(red): should_Given_Empty_Or_Null_List_Give_Empty_List

--- src/main/java/csvFilter/CsvFilter.java
+++ src/main/java/csvFilter/CsvFilter.java
@@ -1,4 +1,9 @@
 package csvFilter;
 
+import java.util.List;
+
 public class CsvFilter {
+    public String apply(List<String> lines){
+        return "";
+    }
 }
--- src/test/java/csvFilter/CsvFilterTest.java
+++ src/test/java/csvFilter/CsvFilterTest.java
@@ -1,13 +1,36 @@
 package csvFilter;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-class CsvFilterTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+public class CsvFilterTest {
 
+    /*
+    *
+    * Uses cases:
+    *   1. Empty or null list give empty list
+    *   2.
+    *
+    *
+    *
+    */
+
+    private final String HEADER_LINE = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
+    private CsvFilter filter = new CsvFilter();
+    private List<String> emptyDataFile = new ArrayList<>(List.of(HEADER_LINE));
+    private String emptyField = "";
+
+    @Test
+    public void should_Given_Empty_Or_Null_List_Give_Empty_List(){
+        assertThat(filter.apply(List.of())).isEqualTo(List.of());
+    }
 
 
 }
